c775d68572768ffbd12fc3e64a83779120bc7a1c
uliang <amisleo000@gmail.com>
2023-08-03 03:15:25 +0800

    刪除沒用的

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ import { Router } from 'express';
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const { id: userId, displayName, photos } = req.session?.passport?.user || {};
-    const photoURL = photos?.[0]?.value;
-    const classes = await getClasses(userId);
-    const schools = await getSchools();
-
-    res.render('index', { displayName, photoURL, classes, schools });
+    res.render('index');
 });
 
 export default router;
